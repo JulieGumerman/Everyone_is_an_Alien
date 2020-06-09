3af1b045116d064cffab4e2a5ec980dbace0f872
bodhibrdr <julie.gumerman@gmail.com>
2020-06-09 17:17:53 -0600

    basic app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // selector: 'app-root',
+  selector: 'reeeeet',
   templateUrl: './app.component.html',
+  // template: `
+  //   <div>
+  //     <h1>{{pageTitle}}</h1>
+  //     <h3>{{quote}}</h3>
+  //   </div>
+  // `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularForms';
+  pageTitle: string = "I is smart; I is kind; I is important";
+  quote: string = "Just because you are small does not mean you are insignificant";
 }
