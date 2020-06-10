4d2c4702f30c47fd2a39b3ba3074cf531ab57191
bodhibrdr <julie.gumerman@gmail.com>
2020-06-09 20:32:20 -0600

    images

--- src/app/quotes/quote-list.component.ts
+++ src/app/quotes/quote-list.component.ts
@@ -12,7 +12,7 @@ export class QuoteListComponent {
         {
             "id": 1,
             "character": "bear",
-            "image": "../img/paw-claws-duotone.svg",
+            "image": "assets/img/paw-claws-duotone.svg",
             "quote": "What if bees are misunderstood? What if bees don't actually want to sting you?",
             "topic": "Misconceptions",
             "pageNumber": 1
@@ -20,6 +20,7 @@ export class QuoteListComponent {
         {
             "id": 2,
             "character": "narrator",
+            "image": "assets/img/book-heart-regular.svg",
             "quote": "When two aliens find each other in a strange place, it feels a little more like home.",
             "topic": "Friendship",
             "pageNumber": 1
@@ -27,6 +28,7 @@ export class QuoteListComponent {
         {
             "id": 3,
             "character": "owl",
+            "image": "assets/img/earlybirds-brands.svg",
             "quote": "I feel like one day I'll meet another owl and they'll take a look at me and say, 'Oh, somebody made a mistake; you're not supposed to be an owl at all.",
             "topic": "Imposter Syndrome",
             "pageNumber": 5
@@ -34,6 +36,7 @@ export class QuoteListComponent {
         {
             "id": 4,
             "character": "tree",
+            "image": "assets/img/tree-alt-duotone.svg",
             "quote": "A friend is anyone or anything who shares a life with you that you would never be able to experience without them.",
             "topic": "Friendship",
             "pageNumber": 1
